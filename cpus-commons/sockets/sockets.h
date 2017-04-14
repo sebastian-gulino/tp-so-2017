407656410eea7fcbfef15d202c78685394dcb6ad
Eduardo Beghe <edubeghe@gmail.com>
2017-04-14 15:16:11 -0300

    Agrego la libreria del grupo

---
+++ cpus-commons/sockets/sockets.h
@@ -0,0 +1,22 @@
+/*
+ * sockets.h
+ *
+ *  Created on: 14/4/2017
+ *      Author: utnso
+ */
+
+#include <stddef.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#ifndef SOCKETS_SOCKETS_H_
+#define SOCKETS_SOCKETS_H_
+
+char * empaquetar(char letraProceso, char * mensaje);
+
+char * desempaquetar(char * mensajeEmpaquetado);
+
+char * procesoEmisor(char * mensajeEmpaquetado);
+
+#endif /* SOCKETS_SOCKETS_H_ */
