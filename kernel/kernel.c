77f33ef1808a1f538c0738422be597805f972e45
Tomas Altrui <altruitomas@gmail.com>
2017-04-18 15:52:13 -0300

    Implementado SELECT en kernel y replicación de mensajes (sin uso de listas)

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -28,89 +28,112 @@ typedef struct config_t {
 
 } t_configuracion;
 
-typedef struct config_clientID{
-
-	int fd;
-	char * cosa;
-} s_clientID;
-
 t_configuracion configuracion;
 
 
-//socket_desc: Socket del servidor.
-//client_sock: Socker del cliente a aceptar.
-//c: Tamaño de la estructura del socket cliente.
-//read_size: Tamaño del mensaje leido.
-//*new_sock: Socket del cliente, parametro para la creación del thread.
-int socket_desc , client_sock , c , read_size, *new_sock, i;
+//socket_cliente: Socker del cliente a aceptar.
+//fdmax: número máximo de descriptores de fichero.
+int socket_cliente, fdmax, i, j, addrlen, recvBytes;
 
-s_clientID clientes[5];
-
+char * buffer_mens_cliente[500];
 //server: Direcciones del server (puerto, ip, etc).
 //client: Direcciones del cliente.
-struct sockaddr_in server , client;
+struct sockaddr_in server, client;
 
-////Buffer donde se almacena el mensaje del cliente
-char mens_cliente[500];;
-
-void *atender_cliente(void *);
-
-int recibirConexiones (int socketServidor){
-	while (1) {
-
-			struct sockaddr_storage addr;
-			socklen_t addrlen = sizeof (addr);
-
-			int clienteConectado = accept(socketServidor, (struct sockaddr *) &addr, &addrlen);
-
-			if (clienteConectado<0){
-				perror("accept");
-				return EXIT_FAILURE;
-			}
-			
-			pthread_t hiloCliente;
-
-			pthread_create(&hiloCliente, NULL, atender_cliente, (void *) clienteConectado);
-		}
-
-}
+//master: conjunto maestro de descriptores de fichero
+//read_fds: conjunto temporal de descriptores de fichero para select()
+fd_set master, read_fds;
 
 
-void *atender_cliente(void *arg)
-{
-	int cl = (long)arg; //Socket cliente aceptado
-	int tam_mens; //Tamaño del return de recv() (tamaño del mensaje recibido)
 
 
+int recibirConexiones_select (int socketServidor){
+
+	FD_ZERO(&master);    // borra los conjuntos maestro y temporal
+	FD_ZERO(&read_fds);
+
+	// añadir servidor al conjunto maestro
+	FD_SET(socketServidor, &master);
+	// seguir la pista del descriptor de fichero mayor
+	fdmax = socketServidor;
 
 	while(1){
-		int j;
 
-		tam_mens = recv(cl, mens_cliente, 20, 0); //Recibe mensaje del cliente (cl)
-		puts(mens_cliente);
+		read_fds = master; // se copia el master al temporal
 
-		for(j = 0; j<5; j++){
-			write(clientes[j].fd, mens_cliente, sizeof(mens_cliente));
+		if (select(fdmax+1, &read_fds, NULL, NULL, NULL) == -1) {
+		 perror("select");
+		   exit(1);
+		 }
+
+		for(i = 0; i <= fdmax; i++) {
+			if (FD_ISSET(i, &read_fds)) { //testea si un file descriptor es parte del set
+				 if (i == socketServidor) {
+
+					 addrlen = sizeof(client);
+
+					 socket_cliente = accept(socketServidor, (struct sockaddr *)&client, &addrlen); //Acepta una conexión
+					  //TODO: Hacer el handshake
+					 if (socket_cliente == -1) {
+					  perror("accept");
+					 }	else {
+						 FD_SET(socket_cliente, &master); // añadir al conjunto maestro
+
+						 if (socket_cliente > fdmax) {    // actualizar el máximo
+						 fdmax = socket_cliente;
+					 }
+						 printf("Kernel: Nueva conexión de la IP %s en el " "socket %d\n", inet_ntoa(client.sin_addr), socket_cliente);
+					 }
+			} else {
+				 // Recibe de un cliente
+				  if ((recvBytes = recv(i, buffer_mens_cliente, sizeof(buffer_mens_cliente), 0)) <= 0) {
+					  if (recvBytes == 0) {
+					  // conexión cerrada
+					  printf("Kernel: socket %d ha cerrado la comunicación\n", i);
+					  } else {
+					       perror("recv");
+					   	  }
+					  close(i); // Se cierra la comunicación con el socket
+					  FD_CLR(i, &master); // elimina el socket cliente del conjunto maestro
+
+
+				  } else { // Se recibieron datos
+
+					  for(j = 0; j <= fdmax; j++) { //Replicación del mensaje
+						  // TODO: Hacer la replicación con listas para poder manejar a quien le mando los mensajes.
+						  if (FD_ISSET(j, &master)) {
+
+							 if (j != socketServidor && j != i) { //No se lo envío ni al servidor ni al cliente que mando al mensaje
+
+								if (send(j, buffer_mens_cliente, recvBytes, 0) == -1) {
+								   perror("send");
+							  }
+							}
+						 }
+					  }
+				   }
+
+			 }
 		}
 
-		if (tam_mens == -1){ //Reconoce error al recibir
-			perror("No se pudo recibir mensaje\n");
-			return EXIT_FAILURE;
-		}
-	}
+    } //end_for
 
 
-
-	return NULL;
-
-	}
-
+  }//end_while
+	return 0;
+}//end recibirConexiones_select
 
 
 void cargarConfiguracion(void) {
 	t_config * config;
 
 	config = config_create("./config.txt");
+
+	if(config == NULL){
+
+		config = config_create("../config.txt");
+
+	}  //TODO: Encontrar una forma menos villera de hacer esto. Seba dixit.
 
 	configuracion.puertoCpu = strdup(config_get_string_value(config, "PUERTO_CPU"));
 	configuracion.ipMemoria = strdup(config_get_string_value(config, "IP_MEMORIA"));
@@ -126,36 +149,22 @@ void cargarConfiguracion(void) {
 	configuracion.semINIT = strdup(config_get_string_value(config, "SEM_INIT"));
 	configuracion.sharedVars = strdup(config_get_string_value(config, "SHARED_VARS"));
 	configuracion.stackSize = strdup(config_get_string_value(config, "STACK_SIZE"));
-<<<<<<< HEAD
-=======
-}
-
-int verificarCliente(char*mensaje){
-
-		puts("Credenciales verificadas...");
-		return 0;
-
-}
-
-int main(int arc, char * argv[]) {
-
-	cargarConfiguracion();
->>>>>>> 0a8ddf84ea78c834b6603a81c521b7c4e37c9ef7
 
 	printf("El puerto de la CPU es %s\n",configuracion.puertoCpu);
-	printf("La IP de la Memoria es %s\n",configuracion.ipMemoria);
-	printf("El puerto de la Memoria es %s\n",configuracion.puertoMemoria);
-	printf("El puerto del Programa es %s\n",configuracion.puertoProg);
-	printf("La IP del FS es %s\n",configuracion.ipFS);
-	printf("El puero del FS es %s\n",configuracion.puertoFS);
-	printf("El Quantum es %s\n",configuracion.quantum);
-	printf("EL Quantum Sleep es %s\n",configuracion.quantumSleep);
-	printf("El Algoritmo es %s\n",configuracion.algoritmo);
-	printf("El grado multiprog es %s\n",configuracion.gradoMultiprog);
-	printf("La Sem IDS es %s\n",configuracion.semIDS);
-	printf("La Sem INIT es %s\n",configuracion.semINIT);
-	printf("Las variables compartidas son %s\n",configuracion.sharedVars);
-	printf("El tamaño del Stack es %s\n",configuracion.stackSize);
+		printf("La IP de la Memoria es %s\n",configuracion.ipMemoria);
+		printf("El puerto de la Memoria es %s\n",configuracion.puertoMemoria);
+		printf("El puerto del Programa es %s\n",configuracion.puertoProg);
+		printf("La IP del FS es %s\n",configuracion.ipFS);
+		printf("El puero del FS es %s\n",configuracion.puertoFS);
+		printf("El Quantum es %s\n",configuracion.quantum);
+		printf("EL Quantum Sleep es %s\n",configuracion.quantumSleep);
+		printf("El Algoritmo es %s\n",configuracion.algoritmo);
+		printf("El grado multiprog es %s\n",configuracion.gradoMultiprog);
+		printf("La Sem IDS es %s\n",configuracion.semIDS);
+		printf("La Sem INIT es %s\n",configuracion.semINIT);
+		printf("Las variables compartidas son %s\n",configuracion.sharedVars);
+		printf("El tamaño del Stack es %s\n",configuracion.stackSize);
+
 
 }
 
@@ -167,7 +176,7 @@ int main(int arc, char * argv[]) {
 
 	int socketServer = crearServidor();
 
-	recibirConexiones(socketServer);
+	recibirConexiones_select(socketServer);
 
 	return 0;
 
