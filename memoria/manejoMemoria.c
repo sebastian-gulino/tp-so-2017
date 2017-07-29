93375cb9bc95281eeb5dc941f217169f1b217e15
Eduardo Beghe <edubeghe@gmail.com>
2017-07-29 02:51:47 -0300

    Arreglo algunas cosas de la consola de memoria

--- memoria/manejoMemoria.c
+++ memoria/manejoMemoria.c
@@ -658,9 +658,9 @@ void aplicarRetardo(){
 }
 
 void establecerRetardoMemoria(int cantidad){
-	retardoLecturaMemoria = cantidad;
+	configuracion->retardoMemoria = cantidad;
 	pthread_mutex_lock(&mutex_log);
-	log_info(logger,"Se cambio el retardo en milisegundos a d%.", retardoLecturaMemoria);
+	log_info(logger,"Se cambio el retardo en milisegundos a d%.", configuracion->retardoMemoria);
 	pthread_mutex_unlock(&mutex_log);
 }
 
@@ -797,6 +797,8 @@ void dumpMemoria(){
 		fprintf(f,"PID: %d\n",list_get(procesosActivos,i));
 	}
 
+	fprintf(f,"(Bonus Track) Retardo de Memoria: %d milisegundos.\n", configuracion->retardoMemoria);
+
 	fclose(f);
 
 }
@@ -813,13 +815,13 @@ void manejoConsola(){
 
 	while(consolaConectada){
 
-		puts("Ingrese algún comando no mayor a 50 caracteres");
+		puts("Ingrese algún comando no mayor a 50 caracteres\n");
 		char * value = malloc(50);
 		scanf("%s", value);
 
 		switch(commandParser(value)){
 			case 1:;
-				puts("Ingrese el nuevo retardo en milisegundos...");
+				puts("Ingrese el nuevo retardo en milisegundos...\n");
 				char * path = malloc(200);
 				scanf("%s", path);
 
@@ -854,12 +856,14 @@ void manejoConsola(){
 					printf("PID: %d\n",list_get(procesosActivos,i));
 				}
 
+				printf("(Bonus Track) Retardo de Memoria: %d milisegundos.\n", configuracion->retardoMemoria);
+
 				dumpMemoria();
 
 				break;
 			case 3:
 
-				puts("Ingrese opción MEMORY o PID...");
+				puts("Ingrese opción MEMORY o PID...\n");
 				char * otro = malloc(200);
 				scanf("%s", otro);
 
@@ -876,7 +880,7 @@ void manejoConsola(){
 
 						break;
 					case 7:
-						puts("Ingrese PID");
+						puts("Ingrese PID\n");
 						char * pid = malloc(200);
 						scanf("%s", pid);
 
